d138163ab5ff6f7ba453eefb8874b30fa985c532
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2017-01-24 17:32:27 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# React redux form example
+
+This is an example of the application that uses react, redux, react-redux-form,
+webpack, webpack-dev-server, babel, etc...
---
+++ config/webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require("webpack");
+var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var rootDir = path.dirname(__dirname);
+
+module.exports = {
+  context: rootDir,
+  entry: './src/index.js',
+  output: {
+    path: path.join(rootDir, 'build'),
+    filename: 'index.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+};
